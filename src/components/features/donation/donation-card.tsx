c8f453b0feced1f873b48b1c1ca03ec82a2a5575
Makene Neto <makeneto17@gmail.com>
2026-08-02 21:37:18 +0100

    feat: refactor DonationCard styles for improved responsiveness and visual consistency

--- src/components/features/donation/donation-card.tsx
+++ src/components/features/donation/donation-card.tsx
@@ -7,12 +7,19 @@ export default function DonationCard() {
   return (
     <Link
       href="/donate"
-      className="bg-[#ffb70050] dark:bg-[#1d190f] hover:bg-[#ffb70070] dark:hover:bg-[#19150d] transition-colors rounded-lg grid gap-1 p-3.5"
+      className="
+        bg-[#dcd0b4] dark:bg-[#2b2007]  
+        hover:bg-[#c2b8a1] dark:hover:bg-[#322508] 
+
+        md:bg-[#ffb70050] dark:md:bg-[#19150d]
+        md:hover:bg-[#ffb70070] md:dark:hover:bg-[#221a05] 
+
+        transition-colors rounded-lg grid gap-1 p-3.5"
     >
       <h1 className="text-sm font-medium text-[#664d06] dark:text-[#d0b260]">
         {t("donate.titleCard")}
       </h1>
-      <p className="text-xs text-[#6b5f3fbc] dark:text-[#b2aa94]">
+      <p className="text-xs text-[#362d13bc] dark:text-[#b2aa94] md:text-[#6b5f3fe3] ">
         {t("donate.descriptionCard")}
       </p>
     </Link>
