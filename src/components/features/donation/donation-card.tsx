67a6a1b14ec261bd9572f0c0225ef8bb471eabd9
Makene Neto <makeneto17@gmail.com>
2026-08-02 17:25:16 +0100

    feat: update DonationCard to use translation hooks for dynamic text

--- src/components/features/donation/donation-card.tsx
+++ src/components/features/donation/donation-card.tsx
@@ -1,15 +1,19 @@
+import { useTranslation } from "@/hooks/use-translation"
 import Link from "next/link"
 
 export default function DonationCard() {
+  const { t } = useTranslation()
+
   return (
     <Link
       href="/donate"
       className="bg-[#ffb70050] dark:bg-[#1d190f] hover:bg-[#ffb70070] dark:hover:bg-[#19150d] transition-colors rounded-lg grid gap-1 p-3.5"
     >
-      <h1 className="text-sm font-medium text-[#664d06] dark:text-[#d0b260]">Donation</h1>
+      <h1 className="text-sm font-medium text-[#664d06] dark:text-[#d0b260]">
+        {t("donate.titleCard")}
+      </h1>
       <p className="text-xs text-[#6b5f3fbc] dark:text-[#b2aa94]">
-        Please consider supporting my work by donating. Your support helps me
-        continue creating and maintaining this and others free projects.
+        {t("donate.descriptionCard")}
       </p>
     </Link>
   )
